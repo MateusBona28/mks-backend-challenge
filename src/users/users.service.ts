4a9d758ac94122cc6bcbefd876107e2dbc8d2a1f
mateus <mateusbona28@gmail.com>
2022-11-30 19:50:11 -0300

    feat: rota para criação de usuário e suas devidas validações finalizada.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    throw new Error('Method not implemented.');
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { username, password } = createUserDto;
+
+    const user = {
+      username,
+      password,
+    };
+
+    this.usersRepository.create(user);
+    const newUser = await this.usersRepository.save(user);
+
+    return newUser;
+  }
+
+  async findUserByUsername(username: string) {
+    return await this.usersRepository.findOneBy({ username });
   }
 }
