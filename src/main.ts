8a3bbae4416d40a8dab90cd4d46cdab1c4a77e01
mateus <mateusbona28@gmail.com>
2022-12-01 00:30:46 -0300

    feat: iniciado a documentação do projeto com a biblioteca swagger, endpoints cobertos: create user

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('MKS-BACKEND-CHALLENGE')
+    .setDescription(
+      'Você pode achar esta API no repositório: https://github.com/MateusBona28/mks-backend-challenge.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,16 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'Desenvolvedor',
+  })
   @IsString()
   username: string;
 
+  @ApiProperty({
+    example: '987',
+  })
   @IsString()
   password: string;
 }
